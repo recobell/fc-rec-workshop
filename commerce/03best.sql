5b4819222efd225b42d0a6b7ae8f02138d984598
zinc <zinc.park@recobell.com>
2017-08-14 21:45:01 +0900

    add examples

--- commerce/03best.sql
+++ commerce/03best.sql
@@ -85,8 +85,60 @@ from (
 
 
 -- 5. conversion_rate 를 이용한 best 만들기
--- 6. category2 best 만들기
+drop table if exists view_stats;
 
+create table view_stats as
+select
+	item_id,
+	count(*) as view_cnt
+from view_log
+group by item_id
+
+drop table if exists product_best_conv;
+
+create table product_best_conv as
+select
+	item_id,
+	view_cnt,
+	order_cnt,	
+	conv_rate,
+	row_number() over (order by conv_rate desc) as rank
+from (
+	select
+		a.item_id,
+		view_cnt,
+		order_cnt,
+		(order_cnt::float/view_cnt::float)::float conv_rate
+	from view_stats a
+	inner join (
+		select
+			item_id,
+			count(*) as order_cnt
+		from order_log
+		group by item_id
+		having count(*) > 3
+	) b on a.item_id = b.item_id
+) a;
+
+
+
+-- 6. category2 best 만들기
+drop table if exists category2_best;
+
+create table category2_best as
+select 
+	category,
+	item_id,
+	score,
+	row_number() over (partition by category order by score desc) rank
+from ( 
+	select
+		b.category2 category,
+		a.item_id,
+		a.score
+	from product_best a
+	inner join product b on a.item_id = b.item_id
+) a;
 
 
 
