7b53e5571f501c0a0b6ccfe3037c3c5f4a14331c
jonghm <jonghm@recobell.com>
2018-01-19 11:56:53 +0900

    append Execise 3.1 and Postgres Installation Guide

--- init-db.sql
+++
@@ -1,8 +0,0 @@
--- Connect to your local database using the following command
---$ psql postgres
-
-create user fcuser with password 'fcuser123';
-
-create database fcrec;
-
-grant all privileges on database fcrec to fcuser;
